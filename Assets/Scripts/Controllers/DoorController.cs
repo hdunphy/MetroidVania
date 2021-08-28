2c215f7e1df6b36c779f2ad17d4248afbfc19098
Henry Dunphy <hdunphy15@gmail.com>
2021-08-28 06:50:29 -0400

    Finished Door Switching
    Added art/animations for Door and switch.

--- Assets/Scripts/Controllers/DoorController.cs
+++ Assets/Scripts/Controllers/DoorController.cs
@@ -5,9 +5,18 @@ using UnityEngine;
 
 public class DoorController : MonoBehaviour
 {
+    [SerializeField] private Animator Animator;
+
     public void Open()
     {
         //Animate
+        Animator.enabled = true;
+        //Animator.SetTrigger("OpenDoor");
+        //gameObject.SetActive(false);
+    }
+
+    public void SetObjectInactive()
+    {
         gameObject.SetActive(false);
     }
 }
--- Assets/Scripts/Controllers/DoorSwitchController.cs
+++ Assets/Scripts/Controllers/DoorSwitchController.cs
@@ -5,15 +5,22 @@ using UnityEngine;
 public class DoorSwitchController : MonoBehaviour
 {
     [SerializeField] private DoorController Door;
+    [SerializeField] private SpriteRenderer SpriteRenderer;
+    [SerializeField] private Sprite OffSprite;
+    [SerializeField] private Sprite OnSprite;
+    [SerializeField] private Animator Animator;
 
     public void OnDoorSwitched()
     {
         FindObjectOfType<Damageable>().enabled = false;
         //Animate switch moving to on position
+        Animator.enabled = true;
         Door.Open();
+    }
 
-
-        //for testing
-        transform.GetComponentInChildren<SpriteRenderer>().color = Color.green;
+    public void SetDoorSwitchOnState()
+    {
+        Animator.enabled = false;
+        SpriteRenderer.sprite = OnSprite;
     }
 }
