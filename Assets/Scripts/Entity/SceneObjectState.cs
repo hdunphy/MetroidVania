1cb9313b3847f0bd492b57e7f4e0e4c567c85a3b
Henry Dunphy <hdunphy15@gmail.com>
2021-09-02 08:42:08 -0400

    On Validate for GUID
    Checking GUIDs to make sure there are not two of the same

--- Assets/Scripts/Entity/SceneObjectState.cs
+++ Assets/Scripts/Entity/SceneObjectState.cs
@@ -8,6 +8,15 @@ public abstract class SceneObjectState : MonoBehaviour
     [SerializeField, Tooltip("GUID used by save system")] protected string GUID;
     protected SceneObjectData SceneObjectData { get; set; }
 
+    private void OnValidate()
+    {
+        if(FindObjectsOfType<SceneObjectState>().Any(x => x.GUID == GUID && x != this))
+        {
+            var duplicate = FindObjectsOfType<SceneObjectState>().First(x => x.GUID == GUID && x != this);
+            Debug.LogWarning($"Two Objects with the same GUID: {GUID}\nOn Object [{name}] and [{duplicate.name}].");
+        }
+    }
+
     /// <summary>
     /// Used by SaveSystem. Updates the Scene Object Data in the Current SaveData Object for the current scene.
     ///     **Assumes that the SceneObjectData has been updated before calling**
