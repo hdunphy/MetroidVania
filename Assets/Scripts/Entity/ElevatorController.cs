d2e7fe68268cdbaf9c89a9ebaa618e1ac83077e8
Henry Dunphy <hdunphy15@gmail.com>
2021-10-16 13:53:14 -0400

    Adding more to Hangar 1 scene

--- Assets/Scripts/Entity/ElevatorController.cs
+++ Assets/Scripts/Entity/ElevatorController.cs
@@ -4,18 +4,27 @@ using UnityEngine;
 public class ElevatorController : MonoBehaviour
 {
     [SerializeField] private float MoveSpeed;
+    [SerializeField] private float PauseTime;
     [SerializeField] private Transform MovePosition;
 
     private int currentIndex;
     private int moveDirection;
+    private float currentPauseTime;
+    private bool atPosition;
+    private Vector3 moveDistance;
     private List<Transform> movePositions;
+    private List<GameObject> connectedEntities;
+
     // Start is called before the first frame update
     void Start()
     {
         moveDirection = 1;
         currentIndex = 0;
+        currentPauseTime = 0;
+        atPosition = false;
 
         movePositions = new List<Transform>();
+        connectedEntities = new List<GameObject>();
 
         foreach(Transform child in MovePosition)
         {
@@ -28,18 +37,41 @@ public class ElevatorController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        moveDistance = Vector3.zero;
+
         if (movePositions.Count > 1)
         {
-            float distance = Vector3.Distance(transform.position, movePositions[currentIndex].position);
-            if (Mathf.Abs(distance) <= 0.1f)
+            if(atPosition && currentPauseTime > 0)
             {
-                UpdateIndex();
+                currentPauseTime -= Time.deltaTime;
             }
             else
             {
-                var nextPosition = Vector3.MoveTowards(transform.position, movePositions[currentIndex].position, MoveSpeed * Time.deltaTime);
-                transform.position = nextPosition;
+                atPosition = false;
+
+                float distance = Vector3.Distance(transform.position, movePositions[currentIndex].position);
+                if (Mathf.Abs(distance) <= 0.1f)
+                {
+                    atPosition = true;
+                    currentPauseTime = PauseTime;
+
+                    UpdateIndex();
+                }
+                else
+                {
+                    var nextPosition = Vector3.MoveTowards(transform.position, movePositions[currentIndex].position, MoveSpeed * Time.deltaTime);
+                    moveDistance = new Vector2((nextPosition - transform.position).x, 0);
+                    transform.position = nextPosition;
+                }
             }
+        }
+    }
+
+    private void LateUpdate()
+    {
+        foreach(var entity in connectedEntities)
+        {
+            entity.transform.position += moveDistance;
         }
     }
 
@@ -52,5 +84,18 @@ public class ElevatorController : MonoBehaviour
         }
 
         currentIndex += moveDirection;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.TryGetComponent(out IEntityMovement entity))
+        {
+            connectedEntities.Add(collision.gameObject);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        connectedEntities.Remove(collision.gameObject);
     }
 }
